4d19a00ac7e2a3396b2dec0f297336fdfd4f4dce
Yu Chin Fabian Lim <flim@sg.ibm.com>
2024-06-21 05:57:05 +0000

    hacked in embedding and linear changes for quick test
    Signed-off-by: Yu Chin Fabian Lim <flim@sg.ibm.com>

--- src/instructlab/dolomite/hf_models/modeling_utils/__init__.py
+++ src/instructlab/dolomite/hf_models/modeling_utils/__init__.py
@@ -10,7 +10,27 @@ from .attention import (
     repeat_key_value,
     split_query_key_value_tensor_for_attention,
 )
-from .embedding import ParameterizedEmbedding
-from .linear import ParameterizedLinear
+# from .embedding import ParameterizedEmbedding
+# from .linear import ParameterizedLinear
+
+from functools import partial
+
+# from torch.nn import Embedding as ParameterizedEmbedding
+# from torch.nn import Linear as ParameterizedLinear
+from torch.nn import Embedding
+from torch.nn import Linear 
+
+class ParameterizedEmbedding(Embedding):
+
+    def __init__(self, *args, **kwargs):
+        kwargs.pop('std')
+        super().__init__(*args, **kwargs)
+
+class ParameterizedLinear(Linear):
+
+    def __init__(self, *args, **kwargs):
+        kwargs.pop('std')
+        super().__init__(*args, **kwargs)
+
 from .normalization import RMSNorm, get_normalization_function
 from .position_embedding import Alibi, RoPE, YaRNScaledRoPE, apply_rotary_pos_emb
